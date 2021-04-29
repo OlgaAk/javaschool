142ab1c2b8c659f5c037e53137445bc40ca4f8d8
olgaaktas <binpolyglotte@gmail.com>
2021-04-29 22:44:59 +0300

    spring security added

---
+++ src/main/java/io/github/olgaak/config/SpringSecurityInitializer.java
@@ -0,0 +1,6 @@
+package io.github.olgaak.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
--- src/main/java/io/github/olgaak/config/WebAppInitializer.java
+++ src/main/java/io/github/olgaak/config/WebAppInitializer.java
@@ -13,6 +13,7 @@ public class WebAppInitializer implements WebApplicationInitializer {
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
         context.register(WebConfig.class);
+        context.register(WebSecurityConfig.class);
         context.setServletContext(servletContext);
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
         dispatcher.setLoadOnStartup(1);
